e117bbc4652b31f4111fcffd48714eeb2afb25ad
AfuhFlynns <flyinnsafuh@gmail.com>
2025-01-16 12:36:26 +0100

    Modify herosection

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,7 +4,7 @@ import CTAButton from "./CTAButton";
 
 const HeroSection = () => {
   return (
-    <section className="flex flex-col items-center justify-start w-full h-auto gap-5 text-center text-gray-900 md:gap-4 dark:text-white paddingY bg-primary-bg-light dark:bg-primary-bg-dark">
+    <section className="flex flex-col items-center justify-start w-full h-auto gap-5 text-center text-gray-900 md:gap-4 dark:text-white paddingY bg-primary-bg-light dark:bg-primary-bg-dark paddingX md:!px-0">
       {/* Heading */}
       {/* Animate the heading repeatedly (changing opacity from 0 to 100 on page load and do some crazy animations) */}
       <motion.h1
@@ -30,7 +30,7 @@ const HeroSection = () => {
       </motion.h1>
 
       {/* Subtext */}
-      <div className="max-w-2xl mx-auto mt-8 dark:text-text-primary-dark text-text-primary-light text-opacity-30 opacity-80">
+      <div className="max-w-2xl px-2 mx-auto mt-8 dark:text-text-primary-dark text-text-primary-light text-opacity-30 opacity-80">
         <p className="text-lg leading-relaxed text-gray-500 dark:text-gray-300 md:text-lg">
           Tired of using expensive intelligent Virtual friends?
           <br />
@@ -48,9 +48,9 @@ const HeroSection = () => {
       </div>
 
       {/* Buttons */}
-      <div className="flex flex-row justify-start items-center w-full md:h-[13%] h-[20%] gap-6 mt-12 md:flex-row md:justify-start">
+      <div className="flex flex-row justify-start items-center w-full md:h-[13%] h-[20%] gap-6 mt-10 md:flex-row md:justify-start">
         {/* Search Section */}
-        <form className="flex flex-col items-start justify-center w-full md:h-full h-[9rem] gap-4 px-3 md:gap-6 md:flex-row md:items-center md:paddingX mb-8 md:mb-0">
+        <form className="flex flex-col items-start justify-center w-full md:h-full h-[9rem] gap-4 px-3 md:gap-6 md:flex-row md:items-center md:paddingX">
           <div className="flex flex-row items-center h-full md:w-[60%] w-full overflow-hidden bg-transparent border border-opacity-20 border-solid shadow-lg dark:bg-neutral-dark-grey-dark dark:shadow-sm text-text-primary-light dark:text-text-primary-dark border-neutral-light-grey-light dark:border-neutral-light-grey-dark dark:border-opacity-30 rounded-2xl relative">
             <input
               type="text"
