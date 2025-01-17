ff20896e188e985b92e057dcd315f01f584bb36a
AfuhFlynns <flyinnsafuh@gmail.com>
2025-01-17 12:22:22 +0100

    Modify app.tsx for previews

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const App: React.FC = () => {
   const [theme, setTheme] = useLocalTheme("light");
 
   useEffect(() => {
-    setTheme("");
+    setTheme("dark");
   }, [setTheme]);
   const loading = false;
   return (
