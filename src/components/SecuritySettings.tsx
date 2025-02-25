323ff18398e438f21ee857178df8bb169117a2f1
AfuhFlynns <flyinnsafuh@gmail.com>
2025-02-25 19:44:57 +0100

    Added check marks and to handle states in the security settings view of user settings page

--- src/components/SecuritySettings.tsx
+++ src/components/SecuritySettings.tsx
@@ -1,19 +1,40 @@
-const ShareChatHistory = () => {
+import { Check } from "lucide-react";
+import { useState } from "react";
+
+const ShareChatHistory = ({
+  onClick,
+  isChecked,
+}: {
+  onClick: () => void;
+  isChecked: boolean | null;
+}) => {
   return (
     <>
       <span>Share your chat history with us</span>{" "}
-      <button className="cta-btn !text-sm md:!text-md !px-3 !shadow-none !rounded-[40px] !flex !flex-row !items-center !justify-center !font-normal !w-[6.2rem] !h-[2.6rem] !gap-1 !border-red-700 !text-red-500 !bg-transparent hover:!bg-red-600 hover:!text-white !bg-opacity-20">
-        Logout
+      <button
+        className="w-[1.2rem] h-[1.2rem] border-[1.4px] border-gray-500 dark:border-gray-400 rounded-sm flex flex-row items-center justify-center"
+        onClick={onClick}
+      >
+        {isChecked && <Check className="w-full h-full" />}
       </button>
     </>
   );
 };
-const ShareUsername = () => {
+const ShareUsername = ({
+  onClick,
+  isChecked,
+}: {
+  onClick: () => void;
+  isChecked: boolean | null;
+}) => {
   return (
     <>
       <span>Share your username with us</span>{" "}
-      <button className="cta-btn !text-sm md:!text-md !px-3 !shadow-none !rounded-[40px] !flex !flex-row !items-center !justify-center !font-normal !w-[6.2rem] !h-[2.6rem] !gap-1 !border-red-700 !text-red-500 !bg-transparent hover:!bg-red-600 hover:!text-white !bg-opacity-20">
-        Logout
+      <button
+        className="w-[1.2rem] h-[1.2rem] border-[1.4px] border-gray-500 dark:border-gray-400 rounded-sm flex flex-row items-center justify-center"
+        onClick={onClick}
+      >
+        {isChecked && <Check className="w-full h-full" />}
       </button>
     </>
   );
@@ -21,6 +42,15 @@ const ShareUsername = () => {
 
 const SecuritySettings: React.FC = () => {
   const LIST = [ShareUsername, ShareChatHistory];
+  const [shareName, setShareName] = useState<null | boolean>(true);
+  const [shareChats, setShareChats] = useState<null | boolean>(true);
+
+  const handleSelectShareName = () => {
+    setShareName((prev) => !prev);
+  };
+  const handleSelectShareChats = () => {
+    setShareChats((prev) => !prev);
+  };
 
   return (
     <div className="flex flex-col justify-start w-full h-full item-center">
@@ -29,7 +59,14 @@ const SecuritySettings: React.FC = () => {
           key={`item-${index}-${Item.name}`}
           className="custom-input !flex !flex-row !items-center !w-full !h-auto !justify-between !border-[1px] !border-x-0 first:!border-t-0 !border-b-0 !px-1 !py-3 !m-0 !rounded-[0px] !border-opacity-10"
         >
-          {<Item />}
+          {
+            <Item
+              onClick={
+                index === 0 ? handleSelectShareName : handleSelectShareChats
+              }
+              isChecked={index === 0 ? shareName : shareChats}
+            />
+          }
         </div>
       ))}
     </div>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,10 @@
 import { HeroSection } from "../components";
+// import {Helmet} from "react-helmet"
 
 const HomePage = () => {
   return (
     <div className="flex flex-col w-full bg-primary-bg-light dark:bg-primary-bg-dark">
+      {/*TODO: Implement helmet SEO optimization here and on other pages */}
       <HeroSection />
     </div>
   );
